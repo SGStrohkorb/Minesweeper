5eef103b184f044ae94cfd95a562eef835497a51
Sam Strohkorb <sgstrohkorb@ksu.edu>
2019-01-04 18:21:04 -0600

    Finished left click functionality

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import numpy as np
-#np.random.seed(42)
+np.random.seed(42)
 
 class Game:
 
     def __init__(self, height=16, width=30, number_of_bombs=99):
         self.height, self.width, self.number_of_bombs = height, width, number_of_bombs
+        self.done = False
 
         empty_grid = np.zeros((self.height, self.width))
         #print(empty_grid)
@@ -59,4 +60,47 @@ class Game:
         self.visible_grid -= 2
         print(self.visible_grid)
 
+        self.actions = empty_grid.copy()
+        self.actions += 1
+
+    def left_mouse_click(self, x, y):
+        """When the player 'clicks' on a point"""
+        value = self.combined_grid[x, y]
+        action_value = self.actions[x, y]
+        #print(value, "HI")
+
+        invalid_action = False
+        if value == -1:
+            self.done = True
+        elif value != 0 and action_value == 1:
+            self.visible_grid[x, y] = value
+            self.actions[x, y] = 0
+        elif value != 0 and action_value == 0:
+            invalid_action = True
+        elif value == 0 and action_value == 1:
+            #self.visible_grid[x, y] = value
+            #for x, y in (x, y):
+            #print(x, y, "X, Y")
+            points = [[x,y]]
+            for X, Y in points:
+                #print(X, Y, points, "start")
+                for i in [X-1, X, X+1]:
+                    for j in [Y-1, Y, Y+1]:
+                        if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                            pass
+                        elif self.combined_grid[i, j] == -1: #if coordinate is a bomb
+                            pass
+                        else:
+                            if self.combined_grid[i,j] == 0:
+                                if [i,j] not in points:
+                                    points.append([i,j])
+                                    #print([i,j], points, "Append")
+                            self.visible_grid[i, j] = self.combined_grid[i, j]
+                #del points[0]
+        print(self.visible_grid)
+        return invalid_action
+
 thing = Game(8, 8, 10)
+thing.left_mouse_click(0,4)
+thing.left_mouse_click(0,1)
+thing.left_mouse_click(0,0)
