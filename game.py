af79b9fdd4a4ce170ee07e942698700eb06c0a93
Sam Strohkorb <sgstrohkorb@ksu.edu>
2019-05-25 21:05:45 -0500

    Made progress on solver, but buggy

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import colorama
 from colorama import Fore, Style #Colors: 'BLACK', 'BLUE', 'CYAN', 'GREEN', 'LIGHTBLACK_EX', 'LIGHTBLUE_EX', 'LIGHTCYAN_EX', 'LIGHTGREEN_EX', 'LIGHTMAGENTA_EX', 'LIGHTRED_EX', 'LIGHTWHITE_EX', 'LIGHTYELLOW_EX', 'MAGENTA', 'RED', 'RESET', 'WHITE', 'YELLOW'
 import numpy as np
+from scipy import ndimage
 
 colorama.init()
-np.random.seed(42)
+np.random.seed(42) #need to remove later
 
 class Game:
     def __init__(self, height=16, width=30, number_of_bombs=99, visual_updates=True):
@@ -217,8 +218,10 @@ class Game:
 
     def solve_step(self):
         old_visible_grid = self.visible_grid.copy()
-        print(self.visible_grid)
+        #print(self.visible_grid)
         #print(self.combined_grid)
+
+        #print()
 
         for X in range(self.height):
             for Y in range(self.width):
@@ -249,10 +252,7 @@ class Game:
                 continue
             break
 
-        print(X, Y)
-
-        if (possible_bombs+known_bombs) != number_of_bombs_local:
-            print("Not solved")
+        #print(X, Y)
 
         for i in [X-1, X, X+1]:
             for j in [Y-1, Y, Y+1]:
@@ -261,36 +261,189 @@ class Game:
                 elif self.visible_grid[i,j] == -2:
                     self.right_mouse_click(i,j)
 
-        self.clear_step()
+        #self.clear_step()
 
         if np.all(np.equal(old_visible_grid.flatten(), self.visible_grid.flatten())):
             print("Not solved")
+            
+            #possible_bombs = self.empty_grid.copy()
+            possible_bombs = []
+            known_number_of_bombs = []
+            for X in range(self.height):
+                for Y in range(self.width):
+                    if self.visible_grid[X,Y] == -2:
+                        for i in [X-1, X, X+1]:
+                            for j in [Y-1, Y, Y+1]:
+                                if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                                    pass
+                                elif self.visible_grid[i,j] > 0:
+                                    #possible_bombs[X,Y] = 1
+                                    if [X, Y] not in possible_bombs:
+                                        possible_bombs.append([X, Y])
+                                    if [i, j] not in known_number_of_bombs:
+                                        known_number_of_bombs.append([i, j])
+                        #print(possible_bombs, X, Y)
+                        #print()
+            #print(possible_bombs)
+            #print(known_number_of_bombs)
 
-        print()
+            known_number_of_bombs_masks = self.empty_grid.copy().tolist() #tolist() because of irregular data structure
+            for X, Y in known_number_of_bombs:
+                known_number_of_bombs_masks[X][Y] = [[], self.visible_grid[X,Y]] #stores possible bomb locations in [0] and number of bombs needed in [1]
+                tmp_mask = np.zeros((3,3)).tolist()
+                for m, i in enumerate([X-1, X, X+1]):
+                    for n, j in enumerate([Y-1, Y, Y+1]):
+                        if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                            pass
+                        elif self.visible_grid[i,j] == -4:
+                            known_number_of_bombs_masks[X][Y][1] -= 1
+                        elif self.visible_grid[i,j] == -2:
+                            tmp_mask[m][n] = 1
+
+                known_number_of_bombs_masks[X][Y][0] = tmp_mask
+
+            #print(known_number_of_bombs_masks[known_number_of_bombs[0][0]][known_number_of_bombs[0][1]])
+
+            n = 0
+            solvable_pairs = []
+            for X, Y in known_number_of_bombs:
+                bombs_needed = known_number_of_bombs_masks[X][Y][1]
+                #print(bombs_needed)
+
+
+                for i in [X-1, X, X+1]:
+                    for j in [Y-1, Y, Y+1]:
+                        if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                            pass
+                        else:
+                            if [i, j] in known_number_of_bombs:
+                                tmp_bombs_needed = known_number_of_bombs_masks[i][j][1]
+                                if (bombs_needed - tmp_bombs_needed) == 1:
+                                    solvable_pairs.append([[X,Y],[i,j]])
+                n += 1
+
+            print(solvable_pairs)
+
+            bombs = []
+            if len(solvable_pairs) != 0:
+                for pair in solvable_pairs:
+                    tmp_grid = self.empty_grid.copy()
+                    X, Y = pair[0]
+                    I, J = pair[1]
+
+                    first_mask = known_number_of_bombs_masks[X][Y][0]
+                    second_mask = known_number_of_bombs_masks[I][J][0]
+
+                    #print(first_mask, second_mask)
+
+                    #going though each mask to get ready to find where the bomb goes
+                    #did this in two steps instead of loop because it's easier
+                    for i in [X-1, X, X+1]:
+                        for j in [Y-1, Y, Y+1]:
+                            if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                                pass
+                            else:
+                                tmp_grid[i,j] += first_mask[i-X+1][j-Y+1] #transition absolute coordinates to mask coordinates
+
+                    for i in [I-1, I, I+1]:
+                        for j in [J-1, J, J+1]:
+                            if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                                pass
+                            else:
+                                tmp_grid[i,j] += second_mask[i-I+1][j-J+1] #transition absolute coordinates to mask coordinates
+
+                    #finding where bomb does
+                    for i in [X-1, X, X+1]:
+                        for j in [Y-1, Y, Y+1]:
+                            if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                                pass
+                            else:
+                                if tmp_grid[i,j] == 1:
+                                    print("Bomb found", i, j)
+                                    bombs.append([i,j])
+                                    self.right_mouse_click(i,j)
+                                    
+
+                    #print(tmp_grid)
+
+
+            if len(bombs) == 0:
+                print("Not solved again")
+                print(known_number_of_bombs)
+                print(known_number_of_bombs_masks)
+
+                current_bomb_count = self.visible_grid.copy().flatten()
+                current_bomb_count = np.sum([1 if x == -4 else 0 for x in current_bomb_count])
+                bombs_left = self.number_of_bombs - current_bomb_count
+                #print(bombs_left)
+
+                spaces_left = len(known_number_of_bombs)
+                #print(spaces_left)
+
+                tmp_mask_grid = self.empty_grid.copy()
+                tmp_placement_grid = self.empty_grid.copy()
+                for X, Y in known_number_of_bombs:
+                    mask = known_number_of_bombs_masks[X][Y][0]
+
+                    tmp_placement_grid[X, Y] = 1
+                    for i in [X-1, X, X+1]:
+                        for j in [Y-1, Y, Y+1]:
+                            if (i < 0) or (j < 0) or (i >= self.height) or (j >= self.width): #if coordinate is off of the grid
+                                pass
+                            else:
+                                #if tmp_mask_grid[i,j] != 1:
+                                tmp_mask_grid[i,j] += mask[i-X+1][j-Y+1] #transition absolute coordinates to mask coordinates
+
+                print(tmp_mask_grid)
+                print(tmp_placement_grid)
+
+                _, number_of_islands = ndimage.label(tmp_placement_grid)
+                print(number_of_islands)
+
+                for i in np.array(range(bombs_left))+1:
+                    #print(i)
+                    pass
 
 
 
+        self.clear_step()
+
+
+
+thing = Game(10, 10, 20, False)
+thing.left_mouse_click(1,1)
+thing.left_mouse_click(5,5)
+thing.left_mouse_click(5,6)
+thing.left_mouse_click(9,9)
+print(thing)
+
+thing.solve_step()
+#print(thing)
+thing.solve_step()
+#print(thing)
+thing.solve_step()
+#print(thing)
+thing.solve_step()
+#print(thing)
+thing.solve_step()
+#print(thing)
+thing.solve_step()
+#print(thing)
+thing.solve_step()
+thing.solve_step()
+thing.solve_step()
+print(thing)
+thing.solve_step()
+print(thing)
+
+
+
+
+'''
 thing = Game(7, 7, 10, False)
 thing.left_mouse_click(1,1)
 thing.left_mouse_click(4,4)
 thing.left_mouse_click(1,6)
-thing.solve_step()
-print(thing)
-thing.solve_step()
-print(thing)
-thing.solve_step()
-print(thing)
-thing.solve_step()
-print(thing)
-thing.solve_step()
-print(thing)
-thing.solve_step()
-print(thing)
-thing.solve_step()
-print(thing)
-
-
-'''
 thing.left_mouse_click(0,3)
 thing.left_mouse_click(1,2)
 thing.right_mouse_click(0,2)
